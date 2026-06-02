a630c71c9adaa69ca81167da05c6e9c77676140a
chenh23 <chenh23@trip.com>
2026-06-02 16:39:10 +0800

    recover min(swa,total)

--- python/sglang/srt/disaggregation/prefill.py
+++ python/sglang/srt/disaggregation/prefill.py
@@ -134,12 +134,11 @@ class PrefillBootstrapQueue:
         self.kv_manager = self._init_kv_manager()
 
         if self.scheduler.tp_worker.is_hybrid_swa:
-            pass
             # FIXME: current SWA allocation allocate full kv cache size in prefill
-            # self.max_total_num_tokens = min(
-            #     self.max_total_num_tokens,
-            #     self.scheduler.tp_worker.model_runner.swa_max_total_num_tokens,
-            # )
+            self.max_total_num_tokens = min(
+                self.max_total_num_tokens,
+                self.scheduler.tp_worker.model_runner.swa_max_total_num_tokens,
+            )
 
     def _init_kv_manager(self) -> CommonKVManager:
         kv_args_class = get_kv_class(self.transfer_backend, KVClassType.KVARGS)
